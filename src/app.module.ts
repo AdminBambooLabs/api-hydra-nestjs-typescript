a6cd9a5bf63d848f6d434c5d7a9314fddb367b63
Jonathan Assunção <jownke@gmail.com>
2025-02-16 18:24:01 -0300

    feat: add simple user CRUD (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
-import { ConfigModule } from "@nestjs/config"
-import { PrismaService } from "./database/prisma.service"
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './database/prisma.service';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,12 +1,23 @@
-import { IsString, IsNotEmpty, Length, IsEmail } from 'class-validator'
+import {
+  IsString,
+  IsNotEmpty,
+  Length,
+  IsEmail,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @IsString()
-    @IsNotEmpty()
-    @Length(3, 30)
-    readonly name: string;
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 30)
+  readonly name: string;
 
-    @IsEmail()
-    @IsNotEmpty()
-    readonly email: string
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
+  readonly password: string;
 }
